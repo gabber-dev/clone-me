324d86c477616af443e7119388d55d32787f811c
Jack Dwyer <jackdwyer@macbookair.local.meter>
2025-04-16 13:26:57 -0700

    update usage token

--- src/actions.ts
+++ src/actions.ts
@@ -5,10 +5,7 @@ export const generateUserToken = async () => {
 
     const response = await axios.post('https://app.gabber.dev/api/v1/usage/token', {
         human_id: crypto.randomUUID(),
-
-        limits: [
-            { type: 'conversational_seconds', value: 500 }
-        ]
+        ttl_seconds: 3600
     }, {
         headers: {
             'X-api-key': `${process.env.GABBER_API_KEY}`
